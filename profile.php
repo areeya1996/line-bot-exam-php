ceac23b819b91ac5b57c1833cb8101b001267769
areeya1996 <39901747+areeya1996@users.noreply.github.com>
2018-06-07 10:07:36 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '629a2078401439477d5e95b36d952247';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Ubb0ff3a4ce7307a400a06046f01293b5';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
